5131bb548bd1b3e42510a1f9e7541759b2de1e71
PoonamChauhan229 <poonam.chauhan229@gmail.com>
2025-02-10 16:34:26 +0530

    deployment

--- src/components/ApiKey/ApiKey.tsx
+++ src/components/ApiKey/ApiKey.tsx
@@ -1,12 +1,39 @@
 import React from 'react';
 import OverviewTemplate from '../StructuredTemplates/OverviewTemplate';
-import apiData from '../../utilis/apiData'; // Import data from apiData.ts
+import apiData from '../../utilis/apiData.json'; // Import data from apiData.json
+
+// Define the structure of a section
+interface Endpoint {
+  title: string;
+  description: string;
+  method: string;
+  url: string;
+  apiKeyRequired?: boolean;
+  borderColor: string;
+  methodColor?: string;
+}
+interface Section {
+  sectionTitle: string;
+  sectionDescription: string;
+  endpoints: Endpoint[];
+}
+
+
+// Define the structure of apiData
+interface ApiData {
+  sections: Section[];
+}
+
+// Type the imported apiData as ApiData
+const apiDataTyped = apiData as ApiData;
 
 const ApiKey: React.FC = () => {
-  // Filter to get only the "Products" section
-  const productSection = apiData.sections.find(section => section.sectionTitle === "API Key");
+  // Filter to get only the "API Key" section
+  const productSection = apiDataTyped.sections.find(
+    (section) => section.sectionTitle === 'API Key'
+  );
 
-  if (!productSection) return null; // In case there's no Products section
+  if (!productSection) return null; // In case there's no "API Key" section
 
   return <OverviewTemplate sections={[productSection]} />;
 };
